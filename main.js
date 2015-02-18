55696c362c4816980d0231851a8079c4484be8e7
Tim JK <timfjk@gmail.com>
2015-02-19 09:10:21 +1300

    add js & css files

---
+++ main.js
@@ -0,0 +1,7 @@
+function showSurname() {
+	document.getElementById("surname").style.visibility = "visible";
+}
+
+function hideSurname() {
+	document.getElementById("surname").style.visibility = "hidden";
+}
